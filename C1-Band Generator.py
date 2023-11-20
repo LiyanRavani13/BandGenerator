c1c3550c8d3f1d7a6486fa78015151102985805a
LiyanRavani13 <151584269+LiyanRavani13@users.noreply.github.com>
2023-11-20 17:13:36 -0600

    Add files via upload

---
+++ C1-Band Generator.py
@@ -0,0 +1,34 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator!")
+
+#2. Ask the user for the city that they grew up in.
+#city_name = input("What city did you grow up in?")
+
+
+#3. Ask the user for the name of a pet.
+#pet = input("What is the name of your pet?")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+# print("Your band name is: "+ city_name + " " + pet)
+# print(f"Your band name is: {city_name} {pet} ")
+
+#5. Make sure the input cursor shows on a new line:
+
+# Primitive datatypes
+# str1 = "Hello"
+
+# print("123" + "456")
+# print(123 + 456)
+# print(123_456_789)
+
+# var = len(input("What is your name?"))
+
+# print("Your name has " +str(var)+ " characters")
+# print(70 + float("100.5"))
+
+int1 = int(input("What is your first number?"))
+int2 = int(input("What is the second number?"))
+
+print(int1 + int2)
+
